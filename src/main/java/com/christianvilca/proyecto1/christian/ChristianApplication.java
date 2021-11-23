d9f919a53504a4e6a4b78b6c7150d56a12ea42b0
Christian Vilca <christian.vilcaq@gmail.com>
2021-11-23 14:28:05 -0500

    Initial

---
+++ src/main/java/com/christianvilca/proyecto1/christian/ChristianApplication.java
@@ -0,0 +1,13 @@
+package com.christianvilca.proyecto1.christian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChristianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChristianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/christianvilca/proyecto1/christian/ChristianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.christianvilca.proyecto1.christian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChristianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

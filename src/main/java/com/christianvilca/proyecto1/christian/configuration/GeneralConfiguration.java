5ae79106dfb78276ffcd1ae7d0ce176a1ecc3453
Christian Vilca <christian.vilcaq@gmail.com>
2021-11-24 11:55:27 -0500

    4.2 - Configuración de datasource con properties y classes

--- src/main/java/com/christianvilca/proyecto1/christian/configuration/GeneralConfiguration.java
+++ src/main/java/com/christianvilca/proyecto1/christian/configuration/GeneralConfiguration.java
@@ -6,8 +6,11 @@ import com.christianvilca.proyecto1.christian.bean.MyBeanWithPropertiesImplement
 import com.christianvilca.proyecto1.christian.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableConfigurationProperties(UserPojo.class)
@@ -29,4 +32,14 @@ public class GeneralConfiguration {
         return new MyBeanWithPropertiesImplement(name, apellido);
     }
 
+    // Configuracion de la base de datos
+    @Bean
+    public DataSource dataSource(){
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:mem:testdb");
+        dataSourceBuilder.username("SA");
+        dataSourceBuilder.password("");
+        return dataSourceBuilder.build();
+    }
 }

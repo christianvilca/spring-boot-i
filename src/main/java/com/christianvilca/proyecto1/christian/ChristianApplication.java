c7799b5ff2e45c8b70953180ffb3a92fa2f7ecbd
Christian Vilca <christian.vilcaq@gmail.com>
2021-11-24 09:34:58 -0500

    3.3 - Uso de properties con ejemplo de generación de POJO

--- src/main/java/com/christianvilca/proyecto1/christian/ChristianApplication.java
+++ src/main/java/com/christianvilca/proyecto1/christian/ChristianApplication.java
@@ -4,6 +4,7 @@ import com.christianvilca.proyecto1.christian.bean.MyBean;
 import com.christianvilca.proyecto1.christian.bean.MyBeanWithDepdency;
 import com.christianvilca.proyecto1.christian.bean.MyBeanWithProperties;
 import com.christianvilca.proyecto1.christian.component.ComponentDependency;
+import com.christianvilca.proyecto1.christian.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -16,12 +17,14 @@ public class ChristianApplication implements CommandLineRunner {
     private MyBean myBean;
     private MyBeanWithDepdency myBeanWithDepdency;
     private MyBeanWithProperties myBeanWithProperties;
+    private UserPojo userPojo;
 
-    public ChristianApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDepdency myBeanWithDepdency, MyBeanWithProperties myBeanWithProperties) {
+    public ChristianApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDepdency myBeanWithDepdency, MyBeanWithProperties myBeanWithProperties, UserPojo userPojo) {
         this.componentDependency = componentDependency;
         this.myBean = myBean;
         this.myBeanWithDepdency = myBeanWithDepdency;
         this.myBeanWithProperties=  myBeanWithProperties;
+        this.userPojo = userPojo;
     }
 
     public static void main(String[] args) {
@@ -34,5 +37,6 @@ public class ChristianApplication implements CommandLineRunner {
         myBean.print();                                         // Hola desde mi implementacion propia del bean 2
         myBeanWithDepdency.printWithDependency();               // Hola desde la implementacion de un bean con dependencia
         System.out.println(myBeanWithProperties.function());    // christian vilca
+        System.out.println(userPojo.getEmail() + "-" + userPojo.getPassword());    // test@mai.com-1234
     }
 }
--- src/main/java/com/christianvilca/proyecto1/christian/configuration/GeneralConfiguration.java
+++ src/main/java/com/christianvilca/proyecto1/christian/configuration/GeneralConfiguration.java
@@ -3,11 +3,14 @@ package com.christianvilca.proyecto1.christian.configuration;
 import com.christianvilca.proyecto1.christian.bean.MyBean;
 import com.christianvilca.proyecto1.christian.bean.MyBeanWithProperties;
 import com.christianvilca.proyecto1.christian.bean.MyBeanWithPropertiesImplement;
+import com.christianvilca.proyecto1.christian.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(UserPojo.class)
 public class GeneralConfiguration {
 
     // @Value() -> llama a los valores o propiedades que hemos inicializado
